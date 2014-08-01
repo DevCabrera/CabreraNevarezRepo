c63ffc1ba291be1b9657c86bb45fb6dcd666cb1b
Garry Cabrera <cabrera82@gmail.com>
2014-08-01 19:17:14 -0400

    Initial Commit

---
+++ Assn4/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Assn4
+//
+//  Created by Garry on 8/1/14.
+//  Copyright (c) 2014 Garry. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
